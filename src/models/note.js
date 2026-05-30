348074d16237c1c111c002343c7e6ea432fad4da
Tymofii Danilov <timadanilov2002@gmail.com>
2026-05-31 00:48:49 +0200

    2nd node.js hw

---
+++ src/models/note.js
@@ -0,0 +1,40 @@
+import { model, Schema } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      required: false,
+      default: 'Todo',
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+const Note = model('note', noteSchema);
+export default Note;
